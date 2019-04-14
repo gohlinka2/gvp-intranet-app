95fa6d2e5f512bb5b2fb2f243ebc24d3cb8e4a4b
gohlinka2 <gohlinka2@gmail.com>
2019-04-14 18:48:25 +0200

    (Not tested yet) Rewrote the parsing algorithm to display formatted text correctly and added documentation.

--- app/src/main/java/cz/hlinkapp/gvpintranet/utils/ArticleNodesParser.kt
+++ app/src/main/java/cz/hlinkapp/gvpintranet/utils/ArticleNodesParser.kt
@@ -9,40 +9,51 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Node
 
+/**
+ * A utility class for parsing html text into an array of [ArticleNode]s.
+ * Explanation: Because embedded images are loaded into a different view than the text, the html document needs to be split into
+ * a list of pieces of text and image source URIs. This list then gets loaded and displayed in a RecyclerView, and the adapter will choose the correct view for each [ArticleNode].
+ * This class handles the creation of the list.
+ */
 class ArticleNodesParser {
 
     private val resultList = ArrayList<ArticleNode>()
-    private val textNodesToAddList = ArrayList<Node>()
+    private val document = Document(ServerContract.BASE_URL)
 
     /**
      * Parses the given html text to a list of [TextNode]s and [ImageNode]s to be displayed in [cz.hlinkapp.gvpintranet.adapters.ArticleNodesRecyclerAdapter] .
      */
     fun parseText (text: String?) : ArrayList<ArticleNode> {
         resultList.clear()
-        textNodesToAddList.clear()
+        document.empty()
         val doc = Jsoup.parseBodyFragment(text)
         doc.setBaseUri(ServerContract.BASE_URL)
-        val nodes = doc.body().childNodesCopy()
-        parse(nodes)
-        writeTextPartsToResult()
+        parse(doc.body()) //parse the document
+        writeTextPartsToResult() //write remaining text parts from working document to result
         return resultList
     }
 
-    //TODO: fix bugs and add docs
-
-    private fun parse(nodes : List<Node>) {
-        for (node in nodes) if (node.childNodesCopy().isEmpty()) {
-            if (node is Element && node.`is`("img")) {
-                writeTextPartsToResult()
-                resultList.add(ImageNode(node.absUrl("src")))
-            } else textNodesToAddList.add(node)
-        } else parse(node.childNodesCopy())
+    /**
+     * Parses the given Node and all of its children recursively into a list of [ArticleNode]s.
+     */
+    private fun parse(nodeToParse : Node) {
+        val shallowNodeClone = nodeToParse.shallowClone()
+        document.appendChild(shallowNodeClone) //appends this node with no children to the working document
+        if (shallowNodeClone is Element) for (childNode in nodeToParse.childNodesCopy()) { //for every child, decide what to do:
+            if (childNode.childNodeSize() == 0) { //if the child does not have children:
+                if (childNode is Element && childNode.`is`("img")) { //if the child is an image, add text from working document to the result and add the image
+                    writeTextPartsToResult()
+                    resultList.add(ImageNode(shallowNodeClone.absUrl("src")))
+                } else shallowNodeClone.appendChild(childNode) //if the child is not an image, append it to the working document (into the parent shallow copy) to be later added to the result
+            } else parse(childNode) //the node has children, parse every child recursively
+        }
     }
 
+    /**
+     * Writes text parts from the working document to the result list. Call before an image is added and after parsing.
+     */
     private fun writeTextPartsToResult() {
-        val textDoc = Document(ServerContract.BASE_URL)
-        textDoc.insertChildren(0,textNodesToAddList)
-        resultList.add(TextNode(textDoc.toString()))
-        textNodesToAddList.clear()
+        resultList.add(TextNode(document.toString()))
+        document.empty() //clear the working document
     }
 }
